89aa5ce69a8ae56b4f618d44a5b6d52ec789ef83
carlosmoreno4470777 <carlosmoreno.4470777@gmail.com>
2024-03-21 21:20:36 -0500

    added global error commit changes

--- pet-store/src/main/java/pet/store/controller/PetStoreController.java
+++ pet-store/src/main/java/pet/store/controller/PetStoreController.java
@@ -2,7 +2,9 @@ package pet.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,16 +19,29 @@ import pet.store.service.PetStoreService;
 @RequestMapping("/pet_store")
 public class PetStoreController {
 
-  @Autowired
-  private PetStoreService petStoreService;
+	@Autowired
+	private PetStoreService petStoreService;
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public PetStoreData createPetStore(@RequestBody PetStoreData petStoreData) {
-    log.info("Received POST request to create a pet store: {}", petStoreData);
-    PetStoreData savedPetStoreData = petStoreService.savePetStore(petStoreData);
-    log.info("Pet store created successfully: {}", savedPetStoreData);
-    return savedPetStoreData;
-  }
-  
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PetStoreData createPetStore(@RequestBody PetStoreData petStoreData) {
+		log.info("Received POST request to create a pet store: {}", petStoreData);
+		PetStoreData savedPetStoreData = petStoreService.savePetStore(petStoreData);
+		log.info("Pet store created successfully: {}", savedPetStoreData);
+		return savedPetStoreData;
+	}
+
+	@PutMapping("/{petStoreId}")
+	public PetStoreData updatePetStore(@PathVariable Long petStoreId, @RequestBody PetStoreData petStoreData) {
+		log.info("Received request to update pet store with ID: {}", petStoreId);
+
+		petStoreData.setPetStoreId(petStoreId); 
+
+		PetStoreData updatedPetStoreData = petStoreService.savePetStore(petStoreData);
+
+		log.info("Pet store with ID: {} updated successfully", petStoreId);
+
+		return updatedPetStoreData;
+	}
+
 }
